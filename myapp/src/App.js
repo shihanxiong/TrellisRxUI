79f13c92e31b5a2dd928f8a245f8606b0d7c849f
Hanxiong Shi <shi@pointbit.net>
2017-08-11 22:50:08 -0400

    Update.

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,26 +2,33 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Projects from './Components/Projects';
+import AddProject from './Components/AddProject';
 
 class App extends Component {
-  constructor () {
+  constructor() {
     super();
     this.state = {
-      projects : [
+      projects: []
+    }
+  }
+
+  componentWillMount() {
+    this.setState({
+      projects: [
         {
-          title : 'Business Website',
-          category : 'Web Design'
+          title: 'Business Website',
+          category: 'Web Design'
         },
         {
-          title : 'Social App',
-          category : 'Mobile Development'
+          title: 'Social App',
+          category: 'Mobile Development'
         },
         {
-          title : 'Ecommerce Shopping Cart',
-          category : 'Web Development'
+          title: 'Ecommerce Shopping Cart',
+          category: 'Web Development'
         }
       ]
-    }
+    });
   }
 
   render() {
@@ -37,7 +44,7 @@ class App extends Component {
         </p>
 
         {/* Application */}
-        <Projects projects = {this.state.projects} />
+        <Projects projects={this.state.projects} />
       </div>
     );
   }
---
+++ myapp/src/Components/AddProject.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AddProject extends Component {
+    render() {
+        return (
+            <div className="Projects">
+                <h3>Add Project</h3>
+            </div>
+        );
+    }
+}
+
+export default AddProject;
